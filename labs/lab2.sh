9a5f769c675c9338316470ea028675f3c465af77
Adam Margherio <amargherio@microsoft.com>
2022-10-31 16:01:34 -0500

    Lab 2 working and tested

--- labs/lab2.sh
+++ labs/lab2.sh
@@ -9,6 +9,7 @@ lab2_build() {
     build_common_infra
 
     # Build our (not really) insecure ARO cluster
+    echo -e "***** Building our ARO lab cluster - this could take up to 30 minutes..."
     az aro create -g $RG_NAME \
         -n $ARO_NAME \
         --vnet $VNET_NAME \
@@ -17,6 +18,7 @@ lab2_build() {
         --location $LOCATION \
         --output none
 
+    echo -e "***** Finishing a few more work items"
     # Generate an NSG that blocks all outbound traffic on the subnets
     nsg_id=$(az network nsg create -g $RG_NAME --name aroNSG --location $LOCATION -o tsv --query id)
     az network nsg rule create --name verysecure \
@@ -31,11 +33,15 @@ lab2_build() {
         --access Deny \
         -o none
 
+    echo -e "***** Just finishing up a few more things...thank you for waiting!"
+    sleep 180
+
     # Assign it to the master subnet
     az network vnet subnet update -n $MASTER_SUBNET \
         -g $RG_NAME \
-        --vnet-name $VNET_NAME
-        --nsg $NSG_NAME
+        --vnet-name $VNET_NAME \
+        --nsg $NSG_NAME \
+        -o none
 
     echo -e "*** Lab number 2 deployment has finished. It looks like we're now unable to get to the master nodes..."
 }
@@ -48,43 +54,23 @@ lab2_validate() {
     echo -e "*** Beginning validation for lab scenario 2..."
 
     # Check the subnet to see if the NSG is even around - if it's gone, then we can pass the validation
-    nsg_id=$(az network vnet subnet show -g $RG_NAME --vnet-name $VNET_NAME -n $MASTER_SUBNET -o tsv --query "networkSecurityGroup.id")
-    if [ -z "${nsg_id}" ]; then
+    subnet_nsg_id=$(az network vnet subnet show -g $RG_NAME --vnet-name $VNET_NAME -n $MASTER_SUBNET -o tsv --query "networkSecurityGroup.id")
+    target_nsg_id=$(az network nsg show -g $RG_NAME -n $NSG_NAME --query id -o tsv)
+    if [ -z "${subnet_nsg_id}" -o "${subnet_nsg_id}" != "${target_nsg_id}" ]; then
         echo -e "*** The NSG has been removed from the subnet - this has restored connectivity!"
         echo -e "***"
         echo -e "*** Congratulations on completing this lab scenarion - you can remove the RG created during this lab."
         return 0
     fi
 
-    # Since the NSG is still applied to the subnet, let's list the outbound rules and see how many we have.
-    #   - If we have more than 1 (when defaults are excluded) then we know there's been more additions made.
-    #   - If we have zero, then the problem rule has been removed.
-    check=$(command -v jq > /dev/null 2>&1)
-
-    if [ check -eq 0]; then
-        # We have `jq' so let's use it
-        rule_id=$(az network nsg rule show -n $RULE_NAME --nsg-name $NSG_NAME -g $RG_NAME --query id -o tsv)
-
-        if [ -z "${rule_id}" && $? -ne 0 ]; then
-            # NSG rule is missing from the NSG because there's no ID and the exit code is non-zero
-            # return successful validation
-            echo -e "*** The deny rule on the NSG has been removed - this has restored connectivity!"
-            echo -e "***"
-            echo -e "*** Congratulations on completing this lab scenarion - you can remove the RG created during this lab."
-            return 0
-        fi
-    else
-        # Doing this the hard way I guess
-        rule_id=$(az network nsg rule show -n $RULE_NAME --nsg-name $NSG_NAME -g $RG_NAME --query id -o tsv)
-
-        if [ -z "${rule_id}" && $? -ne 0 ]; then
-            # NSG rule is missing from the NSG because there's no ID and the exit code is non-zero
-            # return successful validation
-            echo -e "*** The deny rule on the NSG has been removed - this has restored connectivity!"
-            echo -e "***"
-            echo -e "*** Congratulations on completing this lab scenarion - you can remove the RG created during this lab."
-            return 0
-        fi
+    az network nsg rule show -n $RULE_NAME --nsg-name $NSG_NAME -g $RG_NAME --query id -o none
+    if [ $? -ne 0 ]; then
+        # NSG rule is missing from the NSG because there's no ID and the exit code is non-zero
+        # return successful validation
+        echo -e "*** The deny rule on the NSG has been removed - this has restored connectivity!"
+        echo -e "***"
+        echo -e "*** Congratulations on completing this lab scenarion - you can remove the RG created during this lab."
+        return 0
     fi
 
     echo -e "*** Not quite...master connectivity is still disrupted!"
